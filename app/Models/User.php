e0d497c4b5ef387106208762f50a6df356771fc8
abbass <abbas.ajorlou1371@gmail.com>
2026-06-11 16:39:50 +0330

    chore: update .gitignore to exclude cursor files and remove wallet_address from User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,6 @@ class User extends Authenticatable implements MustVerifyEmail, HasMedia
     protected $hidden = [
         'password',
         'remember_token',
-        'wallet_address',
         'nonce',
     ];
 
